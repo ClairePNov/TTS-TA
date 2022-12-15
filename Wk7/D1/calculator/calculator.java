3405858cbf40c7b9c01bf28857a90dff6fd421af
ClairePNov <cep1124@gmail.com>
2022-12-14 23:44:00 -0500

    finished calculator

---
+++ Wk7/D1/calculator/calculator.java
@@ -0,0 +1,27 @@
+public class calculator {
+    
+    //add variable that takes in 2 integer numbers and adds them together
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    //subtract variable that takes in 2 integer numbers and subtracts them together   
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    //multiply variable that takes in 2 integer numbers and multiplies them together
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    //divide variable that takes in 2 integer numbers and divides them together
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    //square variable that takes in 1 integer number and squares it
+    public int square(int num1) {
+        return num1 * num1;
+    }
+}
