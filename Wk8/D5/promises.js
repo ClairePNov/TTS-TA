118a501dbe0f249423eade4651abaa32665c196c
ClairePNov <cep1124@gmail.com>
2022-12-14 23:49:48 -0500

    group async/await

--- Wk8/D5/promises.js
+++ Wk8/D5/promises.js
@@ -1,3 +1,5 @@
+// Async/Await
+//section 1 homework
 async function f() {
 
     let promise = new Promise((resolve, reject) => {
@@ -11,4 +13,36 @@ async function f() {
   
   f();
 
-  
+//group async project
+async function getRandomNumber() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(Math.random());
+      }, 500);
+    });
+   }
+   async function main() {
+    const num = await getRandomNumber();
+    console.log(num);
+   }
+   main();
+   async function fetchCityData(city) {
+    const response = await fetch(
+      `https://geocode.xyz/${city}?json=1`
+    );
+    const data = await response.json();
+    console.log(data.latt, data.longt);
+   }
+   fetchCityData("Shreveport");
+   fetchCityData("Houston");
+
+
+
+
+
+
+
+
+
+
+
